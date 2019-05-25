7edd59dbf8fe4ab2a034fb3b19cf7aacca6e170b
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2019-05-26 03:34:20 +0900

    first commit

---
+++ src/date_format.js
@@ -0,0 +1,37 @@
+Date.prototype.getFormattedDate = function(formatString) {
+
+    formatString = formatString.replace(/yyyy/g, this.getFullYear());
+    formatString = formatString.replace(/MM/g, ("0" + (this.getMonth() + 1)).slice(-2));
+    formatString = formatString.replace(/dd/g, ("0" + this.getDate()).slice(-2));
+    formatString = formatString.replace(/HH/g, ("0" + this.getHours()).slice(-2));
+    formatString = formatString.replace(/mm/g, ("0" + this.getMinutes()).slice(-2));
+    formatString = formatString.replace(/ss/g, ("0" + this.getSeconds()).slice(-2));
+
+    formatString = formatString.replace(/yy/g, ("" + this.getFullYear()).slice(-2));
+    formatString = formatString.replace(/M/g, this.getMonth() + 1);
+    formatString = formatString.replace(/d/g, this.getDate());
+    formatString = formatString.replace(/H/g, this.getHours());
+    formatString = formatString.replace(/m/g, this.getMinutes());
+    formatString = formatString.replace(/s/g, this.getSeconds());
+
+    formatString = formatString.replace(/JPE/g, this.getDayOfWeekStrJP());
+    formatString = formatString.replace(/e/g, this.getDayOfWeekShortStr());
+    formatString = formatString.replace(/E/g, this.getDayOfWeekStr());
+
+    return formatString;
+}
+
+Date.prototype.getDayOfWeekStr = function() {
+    const DAY_OF_WEEK_STR = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return DAY_OF_WEEK_STR[this.getDay()];
+}
+
+Date.prototype.getDayOfWeekShortStr = function() {
+    const DAY_OF_WEEK_STR = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return DAY_OF_WEEK_STR[this.getDay()];
+}
+
+Date.prototype.getDayOfWeekStrJP = function() {
+    const DAY_OF_WEEK_STR = ["日", "月", "火", "水", "木", "金", "土"];
+    return DAY_OF_WEEK_STR[this.getDay()];
+}
